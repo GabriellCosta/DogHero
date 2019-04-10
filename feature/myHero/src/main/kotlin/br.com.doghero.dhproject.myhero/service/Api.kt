1ef6a0609912293ceaab547f1bdaa1a7999f6906
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-10 06:30:10 -0300

    Fix MyHero aPI response type

--- feature/myHero/src/main/kotlin/br.com.doghero.dhproject.myhero/service/Api.kt
+++ feature/myHero/src/main/kotlin/br.com.doghero.dhproject.myhero/service/Api.kt
@@ -1,12 +1,12 @@
 package br.com.doghero.dhproject.myhero.service
 
 import androidx.lifecycle.LiveData
-import br.com.doghero.dhproject.myhero.service.entity.DogHeroUserDTO
+import br.com.doghero.dhproject.myhero.service.entity.MyHeroResponseDTO
 import br.com.doghero.dhproject.network.ui.UiState
 import retrofit2.http.GET
 
 internal interface Api {
 
     @GET("/doghero/myhero")
-    fun fetchHeroes(): LiveData<UiState<DogHeroUserDTO>>
+    fun fetchHeroes(): LiveData<UiState<MyHeroResponseDTO>>
 }
