7823488c5c253d3e8d435d14155ca8f5d3d6b245
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-10 06:45:23 -0300

    Fix error in Repository in MyHero feature

--- feature/myHero/src/main/kotlin/br.com.doghero.dhproject.myhero/service/repository/Repository.kt
+++ feature/myHero/src/main/kotlin/br.com.doghero.dhproject.myhero/service/repository/Repository.kt
@@ -1,10 +1,10 @@
 package br.com.doghero.dhproject.myhero.service.repository
 
 import androidx.lifecycle.LiveData
-import br.com.doghero.dhproject.myhero.service.entity.DogHeroUserDTO
+import br.com.doghero.dhproject.myhero.service.entity.MyHeroResponseDTO
 import br.com.doghero.dhproject.network.ui.UiState
 
 internal interface Repository {
 
-    fun fetchMyHeroes(): LiveData<UiState<DogHeroUserDTO>>
+    fun fetchMyHeroes(): LiveData<UiState<MyHeroResponseDTO>>
 }
