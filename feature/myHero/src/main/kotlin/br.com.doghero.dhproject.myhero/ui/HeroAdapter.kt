3901e05f767373312e85545b12465518a6b537c7
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-16 07:24:52 -0300

    Start a hero list screen creation

---
+++ feature/myHero/src/main/kotlin/br.com.doghero.dhproject.myhero/ui/HeroAdapter.kt
@@ -0,0 +1,50 @@
+package br.com.doghero.dhproject.myhero.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.doghero.dhproject.myhero.R
+import br.com.doghero.dhproject.myhero.ui.vo.HeroVO
+import br.com.doghero.dhproject.myhero.util.bind
+
+internal class HeroAdapter(private val heroList: List<HeroVO>) :
+    RecyclerView.Adapter<HeroAdapter.HeroViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeroViewHolder {
+        val layout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_dog_hero, parent ,false)
+
+        return HeroViewHolder(layout)
+    }
+
+    override fun getItemCount() = heroList.size
+
+    override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
+        val item = heroList[position]
+
+        holder.bind(item)
+    }
+
+    class HeroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val userImage by bind<ImageView>(R.id.img_list_item_hero_photo)
+        private val nameView by bind<TextView>(R.id.txt_list_item_hero_name)
+        private val addressView by bind<TextView>(R.id.txt_list_item_hero_addres)
+
+        fun bind(item : HeroVO) {
+
+            item.apply {
+
+
+
+                nameView.text = item.userName
+                addressView.text = item.address
+            }
+        }
+
+    }
+}
+
--- feature/myHero/src/main/kotlin/br.com.doghero.dhproject.myhero/ui/HeroListActivity.kt
+++ feature/myHero/src/main/kotlin/br.com.doghero.dhproject.myhero/ui/HeroListActivity.kt
@@ -1,10 +1,23 @@
 package br.com.doghero.dhproject.myhero.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.doghero.dhproject.myhero.R
+import br.com.doghero.dhproject.myhero.service.Api
+import br.com.doghero.dhproject.myhero.service.repository.RepositoryImpl
+import br.com.doghero.dhproject.myhero.ui.vo.HeroVO
+import br.com.doghero.dhproject.myhero.util.bind
+import br.com.doghero.dhproject.myhero.viewmodel.MyHeroesViewModel
+import br.com.doghero.dhproject.network.NetworkClient
+import br.com.doghero.dhproject.network.ui.observeOnSuccess
 
 class HeroListActivity : AppCompatActivity() {
+
+    private val recyclerview by bind<RecyclerView>(R.id.rv_hero_list)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +27,27 @@ class HeroListActivity : AppCompatActivity() {
     }
 
     private fun prepareLayout() {
+        val viewModel = MyHeroesViewModel(RepositoryImpl(NetworkClient.getApi(Api::class.java)))
+
+        viewModel.myHeroes.observe(this, Observer {
+            Log.d("HeroListActivity", "Initial observer")
+        })
+
+        viewModel.recents
+            .observeOnSuccess(this, ::recentsSuccess)
+
+        viewModel.favorites
+            .observeOnSuccess(this, ::favoriteSuccess)
+
+        viewModel.fetchHeroes()
+    }
+
+    private fun recentsSuccess(list : List<HeroVO>) {
+        recyclerview.adapter = HeroAdapter(list)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    private fun favoriteSuccess() {
     }
 }
