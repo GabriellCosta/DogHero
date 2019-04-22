2f964a938e077c2180b69f5b4a2e531a2a9d9d3e
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-22 09:37:14 -0300

    Add DI configuration for commons module
    Create a viewmodel delegate to bind viewmodels

---
+++ commons/src/main/kotlin/br/com/doghero/dhproject/commons/di/_commonsModule.kt
@@ -0,0 +1,13 @@
+package br.com.doghero.dhproject.commons.di
+
+import androidx.lifecycle.ViewModelProvider
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
+
+val commonsModule = Kodein.Module("commonsModule") {
+
+    bind<ViewModelProvider.Factory>() with singleton {
+        ViewModelFactory(kodein)
+    }
+}
